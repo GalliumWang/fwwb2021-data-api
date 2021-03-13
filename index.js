43fc5981cd19076ce6668f50776d6d6b166de378
Gallium Wang <galliumwang199@gmail.com>
2021-03-13 23:36:26 +0800

    station api

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-var fs = require('fs'); 
+
+
+var express = require('express');
+var app = express();
 var parse = require('csv-parse');
+var fs = require("fs");
 
-
+var stationData;
 var parser = parse({columns: true}, function (err, records) {
-	console.log(records["编号"]);
-	for(record of records){
-        console.log(record["编号"]);
-    }
+    console.log("station data file loaded");
+    stationData=records;
 });
-
 fs.createReadStream(__dirname+'/data/station.csv').pipe(parser);
+
+app.get('/station', function (req, res) {
+    var stationId=req.query.id;
+    var stationName=req.query.name;
+
+    if(!(stationId^stationName)){
+        res.status(400).send({ error: "invalid parameters numbers!" });
+    }
+    else if(stationId){
+        for(record of stationData){
+            if(record["编号"]==stationId) {
+            res.send(record);
+            return;
+            }
+        }
+        res.status(400).send({ error: "invalid id parameter" });
+    }
+    else{
+        for(record of stationData){
+            if(record["站点名称"]==stationName) {
+                res.send(record);
+                return;
+                }
+        }
+        res.status(400).send({ error: "invalid name parameter" });
+    }
+    res.end();
+    return;
+})
+
+var server = app.listen(2021, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("app listening at http://%s:%s", host, port)
+})
